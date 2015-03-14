57937ec493015ed3358d9a9fa237117de5f321d4
OrSi333 <orsivan22@gmail.com>
2015-03-14 12:38:42 +0200

    Set delay to the Podium

--- Assets/Scripts/PodiumScript.cs
+++ Assets/Scripts/PodiumScript.cs
@@ -8,6 +8,9 @@ public class PodiumScript : MonoBehaviour {
 	public Sprite p1;
 	public Sprite p2;
 	public Sprite p3;
+	private int counter;
+	public int MAX_PODIUM_COUNTER = 30;
+	private bool activateCounter;
 
 	int numPod;
 
@@ -15,17 +18,26 @@ public class PodiumScript : MonoBehaviour {
 	void Start () {
 		podiums = new Sprite[4]{p0,p1,p2,p3};
 		numPod = -1;
-
+		activateCounter = false;
+		counter=0;
 		GetComponent<SpriteRenderer> ().sprite = podiums [0];
 
 	}
 	void OnCollisionEnter2D(Collision2D coll){
-		if (coll.gameObject.tag.Equals ("Player"))
+		if (coll.gameObject.tag.Equals ("Player")&&!activateCounter) {
+			activateCounter = true;
 						hitPodium ();
+				}
 		}
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		if (activateCounter) {
+			counter++;
+			if (counter==MAX_PODIUM_COUNTER){
+				activateCounter=false;
+				counter = 0;
+			}
+				}
 	}
 
 	void hitPodium(){
