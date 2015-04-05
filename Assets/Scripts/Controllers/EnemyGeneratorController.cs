b3e0336dadc1a83316c7a4dfbc3354f4c45d90e4
Omri Keret <omri.keret@gmail.com>
2015-04-05 23:11:27 +0300

    a

---
+++ Assets/Scripts/Controllers/EnemyGeneratorController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyGeneratorController : MonoBehaviour {
+
+	GameObject stupidEnemy ;
+	GenerateWaveLogic generateWaveLogic;
+	public float timeBetweenWaves = 5f;
+	private float fixedTimeStart;
+	// Use this for initialization
+	void Start () {
+		generateWaveLogic = GameObject.Find("Logic").GetComponent<GenerateWaveLogic>();
+		//_leftBodyPart = Instantiate (_leftBodyPartResouce, i_location, Quaternion.identity) as GameObject;
+	}
+	void FixedUpdate(){
+		if (Time.fixedTime - fixedTimeStart > timeBetweenWaves) {
+			GenerateWave();
+			fixedTimeStart = Time.fixedTime;
+		}
+	}
+	void GenerateWave(){
+		generateWaveLogic.generateWave ();
+	}
+}
