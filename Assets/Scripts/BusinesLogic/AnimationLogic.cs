c7bf4c300cc53eb67de8096f33e95b38c637883c
Omri Keret <omri.keret@gmail.com>
2015-04-13 22:47:48 +0300

    add animators

--- Assets/Scripts/BusinesLogic/AnimationLogic.cs
+++ Assets/Scripts/BusinesLogic/AnimationLogic.cs
@@ -4,12 +4,16 @@ using System.Collections;
 public class AnimationLogic : MonoBehaviour
 {
 	public bool faceRight = true;
+	public bool isDashing = false;
 	public Rigidbody2D character ;
-
+	public Animator animator;
 	void Start() {
 		character = GameObject.Find("PlayerManager").GetComponent<Rigidbody2D>();
+		animator = GameObject.Find("PlayerManager").GetComponent<Animator>();
 	}
-
+	void Update() {
+		animator.SetFloat("YVelocity");
+	}
 	public void OnMoveSetDirection(moveAnimationModel model) 
 	{
 		if (model.direction.x > 0 && !faceRight)
@@ -18,6 +22,16 @@ public class AnimationLogic : MonoBehaviour
 			Flip ();
 	}
 
+	public void SetDashing() 
+	{
+		animator.SetBool("s",);
+		isDashing = true;
+	}
+
+	public void UnSetDashing()
+	{
+		isDashing = false;
+	}
 
 	private void Flip()
 	{
