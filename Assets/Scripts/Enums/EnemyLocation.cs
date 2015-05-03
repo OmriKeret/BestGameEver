d55e26847af904fe28f783ae95774a9c53ceb789
OrSi333 <orsivan22@gmail.com>
2015-05-03 20:20:54 +0300

    ש

---
+++ Assets/Scripts/Enums/EnemyLocation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+    public enum EnemyLocation
+    {
+        BottomRight = 1,
+        MidRight,
+        TopRight,
+        TopMid,
+        TopLeft,
+        MidLeft,
+        BottomLeft
+    }
