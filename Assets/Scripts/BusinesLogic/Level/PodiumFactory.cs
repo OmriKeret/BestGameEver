293f1d06c1d29bed20ec0fb93528089d0cc28bd4
Omri Keret <omri.keret@gmail.com>
2015-05-06 10:52:57 +0300

    a

--- Assets/Scripts/BusinesLogic/Level/PodiumFactory.cs
+++ Assets/Scripts/BusinesLogic/Level/PodiumFactory.cs
@@ -4,58 +4,58 @@ using System.Collections.Generic;
 
     public class PodiumFactory
     {
-        private GameObject podiumPrefab;
-        List<GameObject> activePodiums;
+        //private GameObject podiumPrefab;
+        //List<GameObject> activePodiums;
 
-        PodiumModel[][] allLevels;
+        ////PodiumModel[][] allLevels;
 
-        public PodiumFactory()
-        {
-            podiumPrefab = Resources.Load("Podium") as GameObject;
-            activePodiums = new List<GameObject>();
-            allLevels = new PodiumModel[][]{
-                new PodiumModel[]{new PodiumModel(0)},
-                new PodiumModel[]{new PodiumModel(0)},
-                new PodiumModel[]{new PodiumModel(0)},
-                new PodiumModel[]{new PodiumModel(0)}
-            };
-        }
+        ////public PodiumFactory()
+        ////{
+        ////    podiumPrefab = Resources.Load("Podium") as GameObject;
+        ////    activePodiums = new List<GameObject>();
+        ////    allLevels = new PodiumModel[][]{
+        ////        new PodiumModel[]{new PodiumModel(0)},
+        ////        new PodiumModel[]{new PodiumModel(0)},
+        ////        new PodiumModel[]{new PodiumModel(0)},
+        ////        new PodiumModel[]{new PodiumModel(0)}
+        ////    };
+        ////}
 
-        public void SetupNewWave(int i_WaveNumber)
-        {
-            DestroyLevelPodium();
-            //for debug
-            if (!(i_WaveNumber < allLevels.Length))
-            {
-                i_WaveNumber = 0;
-            }
-            //end debug
-            InitLevelPodium(allLevels[i_WaveNumber]);
-        }
+        //public void SetupNewWave(int i_WaveNumber)
+        //{
+        //    DestroyLevelPodium();
+        //    //for debug
+        //    if (!(i_WaveNumber < allLevels.Length))
+        //    {
+        //        i_WaveNumber = 0;
+        //    }
+        //    //end debug
+        //    InitLevelPodium(allLevels[i_WaveNumber]);
+        //}
 
 
-        public void InitLevelPodium(PodiumModel[] i_podiumToInit)
-        {
-            foreach (PodiumModel currentPodiumStats in i_podiumToInit)
-            {
-                GameObject currentPodium = GameObject.Instantiate(podiumPrefab, currentPodiumStats.location, Quaternion.identity) as GameObject;
-                currentPodium.transform.localScale = currentPodiumStats.scale;
-                activePodiums.Add(currentPodium);
-            }
-        }
+        //public void InitLevelPodium(PodiumModel[] i_podiumToInit)
+        //{
+        //    foreach (PodiumModel currentPodiumStats in i_podiumToInit)
+        //    {
+        //        GameObject currentPodium = GameObject.Instantiate(podiumPrefab, currentPodiumStats.location, Quaternion.identity) as GameObject;
+        //        currentPodium.transform.localScale = currentPodiumStats.scale;
+        //        activePodiums.Add(currentPodium);
+        //    }
+        //}
 
-        public void DestroyLevelPodium()
-        {
-            foreach (GameObject currentPodium in activePodiums)
-            {
-                GameObject.Destroy(currentPodium);
-            }
-        }
+        //public void DestroyLevelPodium()
+        //{
+        //    foreach (GameObject currentPodium in activePodiums)
+        //    {
+        //        GameObject.Destroy(currentPodium);
+        //    }
+        //}
 
-        public GameObject createPodium(PodiumModel podiumParams)
-        {
-            GameObject podium = GameObject.Instantiate(podiumPrefab, podiumParams.location, Quaternion.identity) as GameObject;
-            podium.transform.localScale = podiumParams.scale;
-            return podium;
-        }
+        //public GameObject createPodium(PodiumModel podiumParams)
+        //{
+        //    GameObject podium = GameObject.Instantiate(podiumPrefab, podiumParams.location, Quaternion.identity) as GameObject;
+        //    podium.transform.localScale = podiumParams.scale;
+        //    return podium;
+        //}
     }
