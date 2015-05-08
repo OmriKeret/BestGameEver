0760b7440ac9f79feeba4bf26d6252d6201f255f
Omri Keret <omri.keret@gmail.com>
2015-05-08 15:23:46 +0300

    add

---
+++ Assets/Scripts/BusinesLogic/Currency/CurrencyLogic.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CurrencyLogic : MonoBehaviour {
+
+    CurrencyData currencyDataAccess;
+    public int deviderToScore = 100;
+    void Start()
+    {
+        currencyDataAccess = GameObject.Find("GameManagerData").GetComponent<CurrencyData>();
+    }
+
+    public int updateCurrencyByScore(int score)
+    {
+        var currencyToAdd = score / deviderToScore; // + coins
+        var currentCurrency = currencyDataAccess.loadCurrency();
+        currentCurrency.PJ += currencyToAdd;
+        currencyDataAccess.saveCurrency(currentCurrency);
+        return currencyToAdd;
+    }
+}
