4038c47b4a5355c115fffa6504583e0ab34969e5
OrSi333 <orsivan22@gmail.com>
2015-05-11 20:07:41 +0300

    TO OMRI

--- Assets/Scripts/Store/Model/Equipment/Punchoes.cs
+++ Assets/Scripts/Store/Model/Equipment/Punchoes.cs
@@ -7,7 +7,7 @@ public class Punchoes {
 
     public List<ClothModel> pounchoes = new List<ClothModel> {
         new ClothModel{id = 1, description = "Given to him at birth, the Poncho is Ponchjoe's source of awesomeness.", jemPrice = -1, name = "Classic Poncho", owned = true, part = BodyPart.puncho, PJPrice = 0,characterSpriteFront = "Cloth/Poncho/poncho",characterSpriteBack = "Cloth/Poncho/poncho back", selected = true, statsImprove = new StatsImprovementModel{} },
-		new ClothModel{id = 2, description = "Imbued with epic stuff. 'nuff said.", jemPrice = -1, name = "Black Sun Poncho", owned = false, part = BodyPart.puncho, PJPrice = 5,characterSpriteFront = "Cloth/Poncho/poncho2",characterSpriteBack = "Cloth/Poncho/poncho back", selected = false, statsImprove = new StatsImprovementModel{}}
+		new ClothModel{id = 2, description = "Imbued with epic stuff. 'nuff said.", jemPrice = -1, name = "Black Sun Poncho", owned = false, part = BodyPart.puncho, PJPrice = 0,characterSpriteFront = "Cloth/Poncho/poncho2",characterSpriteBack = "Cloth/Poncho/poncho back", selected = false, statsImprove = new StatsImprovementModel{}}
 
         };
 
