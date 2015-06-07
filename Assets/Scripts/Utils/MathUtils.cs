6ec60ce6f30418971ac88e6e1fdf48981c75416b
OrSi333 <orsivan22@gmail.com>
2015-06-07 03:01:21 +0300

    כי

---
+++ Assets/Scripts/Utils/MathUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+
+    public static class MathUtils
+    {
+        public static int Max(int[] i_Array)
+        {
+            int max = int.MinValue;
+            foreach (int i in i_Array)
+            {
+                max = i > max ? i : max;
+            }
+            return max;
+        }
+
+        public static int SumOfArray(int[] i_Array)
+        {
+            int sum = 0;
+            foreach (int i in i_Array)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+
+
