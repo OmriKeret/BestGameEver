ef6ed5a08339a04d89c132c87c402182e7cae262
OmriKeret <omri.keret@gmail.com>
2015-08-01 21:13:18 +0300

    tryed to add scripts

---
+++ Assets/Scripts/BusinesLogic/Level/EndscreenGui/SummeryScreen.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SummeryScreen : MonoBehaviour, PhaseEventHandler {
+
+	Button missionCompleteBtn;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void handleEvent()
+    {
+        //Brings up next button
+    }
+
+    public void next()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void setNext(PhaseEventHandler next)
+    {
+        throw new System.NotImplementedException();
+    }
+}
