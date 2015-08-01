ac63570741fdf63d02b9a191766a17749c47b72b
OmriKeret <omri.keret@gmail.com>
2015-08-01 20:42:59 +0300

    added a script

---
+++ Assets/Scripts/BusinesLogic/Level/EndscreenGui/GUIButtonHelper.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIButtonHelper : MonoBehaviour {
+
+	// level up
+	LevelUpHandler levelupHandler;
+
+	// Mission complete 
+	FinishedMissionHandler finishedMission;
+
+	void Start () {
+		levelupHandler = GameObject.Find("Logic").GetComponentInChildren<LevelUpHandler>();
+		finishedMission = GameObject.Find("Logic").GetComponentInChildren<FinishedMissionHandler>();
+	}
+	
+	public void RewardNext() 
+	{
+		levelupHandler.OnClickRewardNext ();
+	}
+
+	public void FinishedMissionNext()
+	{
+		finishedMission.onNextButtonClicked ();
+	}
+}
